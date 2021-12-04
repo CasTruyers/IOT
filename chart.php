fe7278fec4359e8782187b85eebb30089073ebf8
Cas <cas.truyers@outlook.be>
2021-12-04 18:55:43 +0100

    changed html to php

--- chart.php
+++ chart.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+<header>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.1/chart.min.js" integrity="sha512-O2fWHvFel3xjQSi9FyzKXWLTvnom+lOYR/AUEThL/fbP4hv1Lo5LCFCGuTXBRyKC4K4DJldg5kxptkgXAzUpvA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <title>Chart</title>
-</head>
+    <title>Charts</title>
+</header>
+
 <body>
-<canvas class="myChart"></canvas>
+    <p>TEST</p>
+    <canvas class="myChart" width="400" height="400"></canvas>
+
+    <script>
+
+    </script>
 </body>
+
 </html>
